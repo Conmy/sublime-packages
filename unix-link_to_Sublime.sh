2d4c6e323ace7c35814086599df0efecf76faccb
David Conmy <david.conmy@sap.com>
2018-07-18 18:24:21 +0100

    Moved config files to User folder.
    Added scripts for setup on linux and windows

---
+++ unix-link_to_Sublime.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p ~/.config/sublime-text-3/Packages
+dir=$(pwd)
+ln -s $dir/User ~/.config/sublime-text-3/Packages/User
---
+++ windows-link_to_Sublime.ps1
@@ -0,0 +1,3 @@
+$repo_dir = pwd
+cd "$env:APPDATA\Sublime Text 3\Packages"
+ln -s $repo_dir\User .\User
